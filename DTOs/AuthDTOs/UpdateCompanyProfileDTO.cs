6c56683577053571d497212f17001bb9a7ca7992
Alshaj <bdalrhmnalshaj08@gmail.com>
2026-02-21 04:48:45 +0000

    Make changes in UpdateCompanyProfileDTO

--- DTOs/AuthDTOs/UpdateCompanyProfileDTO.cs
+++ DTOs/AuthDTOs/UpdateCompanyProfileDTO.cs
@@ -2,7 +2,7 @@
 {
     public class UpdateCompanyProfileDTO
     {
-        public string Phone { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
         public string Industry { get; set; } = string.Empty;
         public IFormFile? LogoUrl { get; set; }
         public string CompanyName { get; set; } = string.Empty ;
