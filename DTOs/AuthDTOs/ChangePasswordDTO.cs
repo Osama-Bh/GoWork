601cb99e87d211281b31f9243fcf067a339c6b58
Alshaj <bdalrhmnalshaj08@gmail.com>
2026-02-21 03:18:07 +0000

    Make DeleteAccount, ChangePassword, UpdateProfile endpoints

---
+++ DTOs/AuthDTOs/ChangePasswordDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace GoWork.DTOs.AuthDTOs
+{
+    public class ChangePasswordDTO
+    {
+        public string OldPassword { get; set; } = string.Empty;
+        public string NewPassword { get; set; } = string.Empty;
+    }
+}
