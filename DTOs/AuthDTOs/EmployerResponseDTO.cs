c8b9e38f4ad1437290ebe60a6a96fc75ef5f4fc0
Alshaj <bdalrhmnalshaj08@gmail.com>
2026-02-25 11:54:11 +0000

    handle changing logo in the AccountService

--- DTOs/AuthDTOs/EmployerResponseDTO.cs
+++ DTOs/AuthDTOs/EmployerResponseDTO.cs
@@ -4,7 +4,7 @@
     {
         public int EmployerId { get; set; }
         public string Email { get; set; } = string.Empty;
-        public string SasUrl { get; set; } = string.Empty;
+        public string? SasUrl { get; set; } = string.Empty;
         public DateTimeOffset ExpiresAt { get; set; } = DateTimeOffset.UtcNow;
         public string Role { get; set; } = string.Empty;
         public string CompanyName { get; set; } = string.Empty;
