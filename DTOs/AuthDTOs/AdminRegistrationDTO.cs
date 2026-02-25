65d85396abe8f7b67c8f84b0a8df1bfb7689ab2d
Alshaj <bdalrhmnalshaj08@gmail.com>
2026-02-25 23:03:16 +0000

    Add Admin/Register, Admin/UpdateProfile endpoints

---
+++ DTOs/AuthDTOs/AdminRegistrationDTO.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GoWork.DTOs.AuthDTOs
+{
+    public class AdminRegistrationDTO
+    {
+
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        [DataType(DataType.Password)]
+        public string PasswordConfirmation { get; set; }
+
+        [Required]
+        [Phone]
+        [StringLength(20)]
+        public string PhoneNumber { get; set; }
+
+        //[Url]
+        //public IFormFile? LogoUrl { get; set; }
+    }
+}
