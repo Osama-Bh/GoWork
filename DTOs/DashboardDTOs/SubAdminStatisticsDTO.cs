7392994549b46b883d87133ec6981f70e1e8814a
Alshaj <bdalrhmnalshaj08@gmail.com>
2026-03-01 01:23:21 +0000

    make some changes in AdminService

--- DTOs/DashboardDTOs/SubAdminStatisticsDTO.cs
+++ DTOs/DashboardDTOs/SubAdminStatisticsDTO.cs
@@ -5,5 +5,6 @@
         public int TotalSubAdmins { get; set; }
         public int ActiveSubAdmins { get; set; }
         public int SuspendedSubAdmins { get; set; }
+        public int BlockedSubAdmins { get; set; }
     }
 }
